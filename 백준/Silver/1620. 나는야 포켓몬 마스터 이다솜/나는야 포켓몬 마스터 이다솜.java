418b997909f1a830358c8cfc734d2270ac0bf52f
이승주 <105261571+leesj8116@users.noreply.github.com>
2023-07-19 11:05:17 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 540 ms, Memory: 39180 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
@@ -10,7 +8,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        // String.split => StringTokenizer로 변경
+        // 3차 개선 => 문제 결과 반환을 System.out.println => BufferedWriter 로 변경
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // 1차 개선 => String.split => StringTokenizer로 변경
         StringTokenizer st = new StringTokenizer(bf.readLine());
 
         int N = Integer.parseInt(st.nextToken()); // 포켓몬의 개수 N
@@ -30,16 +31,20 @@ public class Main {
         // 문제 풀이
         for(int i = 0; i < M; i++) {
             String quiz = bf.readLine();    // 문제 입력
-            
             // 2차 개선 => 이름/번호 구분을 익셉션에서 첫글자의 문자/숫자 여부로 변경
             if(Character.isDigit(quiz.charAt(0))) {
                 // 숫자일 경우
-                System.out.println(library[Integer.parseInt(quiz) - 1]);
+                bw.write(library[Integer.parseInt(quiz) - 1]);
             } else {
                 // 숫자가 아닐 경우
-                System.out.println(libMap.get(quiz));
+                bw.write(libMap.get(quiz).toString());  // BufferedWriter로 변경되면서 toString() 호출
             }
+            bw.newLine();
         }
 
+        bw.flush();
+
+//        bw.close();
+//        bf.close();
     }
 }
