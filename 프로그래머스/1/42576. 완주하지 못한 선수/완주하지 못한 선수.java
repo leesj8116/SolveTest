f4188f66ce3675af76af7ef28c24faf6c439f6d7
이승주 <105261571+leesj8116@users.noreply.github.com>
2023-10-10 16:26:53 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 157.31 ms, Memory: 96.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,11 +4,7 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         HashMap<String, Integer> m = new HashMap<>();
         for (String s : completion) {
-            if(m.containsKey(s)) {
-                m.put(s, m.get(s) + 1);
-            } else {
-                m.put(s, 1);
-            }
+            m.put(s, m.getOrDefault(s, 0) + 1);
         }
 
         for(String s : participant) {
